356e4a76785078944d69e263254bc06f60296b67
DavenStilgar <arthurericjunior01@gmail.com>
2025-03-05 03:00:28 +0000

    update commit

--- app/page.js
+++ app/page.js
@@ -3,20 +3,18 @@ import About from "./componets/About";
 import Experience from "./componets/Experience";
 import Project from "./componets/Projects";
 import Contact from "./componets/Contact";
-import Footer from "./componets/Footer";
-import Header from "./componets/Header";
 import Hero from "./componets/Hero";
 
 const page = () => {
   return (
     <div className=" flex flex-col min-h-screen ">
-      <Header/>
+      
       <Hero/>
       <Experience/>
       <Project/>
       <About/>
       <Contact/>
-      <Footer/>
+      
     </div>
    
   );
